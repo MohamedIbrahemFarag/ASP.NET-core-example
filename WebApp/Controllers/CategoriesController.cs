e20769b9418ce13c2fc686a446659251d0ee63a3
PC1 <PC1@DESKTOP-L9IE1LG>
2024-03-03 04:10:42 +0200

    Add project files.

---
+++ WebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = new Category { CategoryId = id ?? 0 };
+
+           return View(category);
+            
+        }
+    }
+}
